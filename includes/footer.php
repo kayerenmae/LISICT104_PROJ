db6cb5c7da9709ff9d8a898997ab9916b3cd799a
kayerenmae <143154332+kayerenmae@users.noreply.github.com>
2023-12-22 11:04:44 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 <a href="https://sites.google.com/view/divinewordcollegelibrary/home">DWCL College Library</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
